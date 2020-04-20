25306c3ad5ef25e6a533881dd047efd5e5990354
Lucas Riccombene <lriccombene@gmail.com>
2020-04-20 20:22:48 -0300

    Creacion de archivo docker-comper.dist

--- bin/first-start.sh
+++ bin/first-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker-compose exec app composer update
-docker-composer exec app chmod 777 ./web/assert -R
+docker-composer exec app chmod 777 ./web/assets -R
 docker-composer exec app chmod 777 ./runtime -R
--- bin/start-project.sh
+++ bin/start-project.sh
@@ -3,3 +3,4 @@
 docker-compuse exec app composer global require "fxp/composer-asset-plugin:^1.4.1"
 
 docker-compuse exec app composer create-project --prefer-dist yiisoft/yii2-app-basic .
+
