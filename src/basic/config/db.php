c7cb18d32c742aeaeb6ed8a748fdaba9044e779f
Lucas Riccombene <lriccombene@gmail.com>
2020-04-20 22:59:00 -0300

    Creacion de la tabla y su contenido USUARIO

--- src/basic/config/db.php
+++ src/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host='.$_ENV['DB_DATABASE'] .';dbname='.$_ENV['DB_PASSWORD'],
+    'dsn' => 'mysql:host='.$_ENV['DB_HOST'].';dbname='.$_ENV['DB_DATABASE'],
     'username' => 'root',
     'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8',
