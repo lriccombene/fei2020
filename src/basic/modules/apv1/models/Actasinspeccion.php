37103629ee1ac1739a1a8538995464a75039f593
Lucas Riccombene <lriccombene@gmail.com>
2020-06-28 19:59:05 -0300

    se implemento axios en view , form, index

--- src/basic/modules/apv1/models/Actasinspeccion.php
+++ src/basic/modules/apv1/models/Actasinspeccion.php
@@ -5,10 +5,10 @@ namespace app\modules\apv1\models;
 class Actasinspeccion extends \app\models\Actasinspeccion
 {
     public function fields (){
-        return ['nro', 'latitud', 'longitud','fec'];
+        return ['nro', 'latitud', 'longitud','fec','localidad', 'categoria', 'motivo', 'empresa', 'area'];
     }
-
+/*
     public function extraFields(){
-        return ['localidad', 'categoria', 'motivo', 'empresa', 'area', ]; 
-    }
+        return [, ]; 
+    }*/
 }
--- src/basic/views/actasinspeccion/_form.php
+++ src/basic/views/actasinspeccion/_form.php
@@ -3,8 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\web\View;
-$this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
-                      ['position'=>View::POS_HEAD]);
+$this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",['position'=>View::POS_HEAD]);
+$this->registerJsFile("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",['position'=>$this::POS_HEAD]);
 /* @var $this yii\web\View */
 /* @var $model app\models\Actasinspeccion */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,11 +12,7 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
 
 <div class="actasinspeccion-form">
 
-
-    <form  id="app"   method="post">
-
-
-    <div class="container-fluid">
+<div  id="app" class="container-fluid">
 	<div class="row">
 		<div class="col-md-2">
             <label for="nro">Nro :</label>
@@ -24,8 +20,9 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
 		<div class="col-md-8">
 
             <input v-bind:placeholder="nro_hint" id="nro" v-model="nro" type="text" name="nro" required >
-            
-		</div>
+            <span class="text-danger" v-if="errors.nro" >{{errors.nro}}</span>
+        </div>
+        
 	</div>
     <div class="row">
 		<div class="col-md-2">
@@ -33,32 +30,36 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
         </div>
         <div class="col-md-8">
             <input v-bind:placeholder="fec_hint"  id="fec" v-model="fec" type="date" name="fec">
+            <span class="text-danger" v-if="errors.fec" >{{errors.fec}}</span>
         </div>
 	</div>
     <div class="row">
 		<div class="col-md-2">
             <label for="localidad">Localidad :</label>
         </div>
-        <div class="col-md-8">
-        <select v-model="selected" required >
+       <div class="col-md-8">
+        <select v-model="selected_localidad" required >
             <option v-for="option in localidades" v-bind:value="option.id">
                 {{ option.nombre }}
             </option>
         </select>
+        <span class="text-danger" v-if="errors.id_localidad" >{{errors.id_localidad}}</span>
         </div>
 	</div>
     <div class="row">
 		<div class="col-md-2">
             <label for="categoria">Categoria :</label>
         </div>
-        <div class="col-md-8">
+       <div class="col-md-8">
         <select v-model="selected_categoria" required >
             <option v-for="option in categorias" v-bind:value="option.id">
                 {{ option.nombre }}
             </option>
         </select>
+        <span class="text-danger" v-if="errors.id_categoria" >{{errors.id_categoria}}</span>
         </div>
 	</div>
+
     <div class="row">
 		<div class="col-md-2">
             <label for="motivo">Motivo :</label>
@@ -69,6 +70,7 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
                 {{ option.nombre }}
             </option>
         </select>
+        <span class="text-danger" v-if="errors.id_motivo" >{{errors.id_motivo}}</span>
         </div>
 	</div>
     <div class="row">
@@ -81,11 +83,12 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
                 {{ option.nombre }}
             </option>
         </select>
+        <span class="text-danger" v-if="errors.id_empresa" >{{errors.id_empresa}}</span>
         </div>
 	</div>
     <div class="row">
 		<div class="col-md-2">
-            <label for="motivo">Motivo :</label>
+            <label for="area">Area :</label>
         </div>
        <div class="col-md-8">
         <select v-model="selected_area" required >
@@ -93,6 +96,7 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
                 {{ option.nombre }}
             </option>
         </select>
+        <span class="text-danger" v-if="errors.id_area" >{{errors.id_area}}</span>
         </div>
 	</div>
     <div class="row">
@@ -101,6 +105,7 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
         </div>
         <div class="col-md-8">
             <input v-bind:placeholder="latitud_hint"  id="latitud" v-model="latitud" type="text" name="latitud">
+            <span class="text-danger" v-if="errors.latitud" >{{errors.latitud}}</span>
         </div>
 	</div>
     <div class="row">
@@ -109,63 +114,187 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",
         </div>
         <div class="col-md-8">
             <input v-bind:placeholder="longitud_hint"  id="longitud" v-model="longitud" type="text" name="longitud">
+            <span class="text-danger" v-if="errors.longitud" >{{errors.longitud}}</span>
         </div>
 	</div>
 
 
     <div class="row">
-		<div class="col-md-2">
-            <input type="submit" value="Enviar" class="btn btn-success">
-        </div>
-    </div>
+      <div class="col-md-2"> 
+              
+             <button v-if="!id"  v-on:click="addActas()"  type ="button"  class="btn btn-success">Enviar</button>
+              <button v-if="id" v-on:click ="editArea(id)" type ="button" class="btn btn-warning" >Actualizar</button>
+              
+          </div>
+      </div>
+    </>
 
-    </form>
+    
+</div>
 </div>
 
 <script>
   var app = new Vue({
                     el:'#app',
                     data:{
+
+                        id:'<?php  echo ($model->id); ?>',
                         nro: '<?php  echo ($model->nro); ?>',
                         nro_hint: 'ingrerse nro',
                         fec:'<?php  echo ($model->fec); ?>',
                         fec_hint: 'ingrerse fec',
-                        selected: '<?php  echo ($model->id_localidad); ?>',
-                        localidades: [
-                                { id: '1', nombre: 'Localidad1' },
-                                { id: '2', nombre: 'Localidad2' },
-                                { id: '3', nombre: 'Localidad3' }
-                               ],
+                        selected_localidad: '<?php  echo ($model->id_categoria); ?>',
+                        localidades:[],
                         selected_categoria: '<?php  echo ($model->id_categoria); ?>',
-                        categorias: [
-                                { id: '1', nombre: 'categoria1' },
-                                { id: '2', nombre: 'categoria2' },
-                                { id: '3', nombre: 'categoria3' }
-                               ],
+                        categorias: [],
                         selected_motivo: '<?php  echo ($model->id_motivo); ?>',
-                        motivos: [
-                                { id: '1', nombre: 'motivo1' },
-                                { id: '2', nombre: 'motivo2' },
-                                { id: '3', nombre: 'motivo3' }
-                               ],
+                        motivos: [ ],
                         selected_empresa: '<?php  echo ($model->id_empresa); ?>',
-                        empresas: [
-                                { id: '1', nombre: 'empresa1' },
-                                { id: '2', nombre: 'empresa2' },
-                                { id: '3', nombre: 'empresa3' }
-                               ],
+                        empresas: [],
                         selected_area: '<?php  echo ($model->id_area); ?>',
-                        areas: [
-                                { id: '1', nombre: 'area1' },
-                                { id: '2', nombre: 'area2' },
-                                { id: '3', nombre: 'area3' }
-                               ],
+                        areas: [],
                         longitud: '<?php  echo ($model->longitud); ?>',
-                        longitud_hint: 'ingrerse longitud',
+                        longitud_hint: 'ingrese longitud',
                         latitud: '<?php  echo ($model->latitud); ?>',
-                        latitud_hint: 'ingrerse latitud',
+                        latitud_hint: 'ingrese latitud',
+                        errors: {},
 
+                    },
+                    mounted() {
+                        this.getLocalidad();
+                        this.getCategorias();
+                        this.getMotivos();
+                        this.getEmpresas();
+                        this.getAreas();
+                        },                 
+                    methods: { 
+                        normalizeErrors: function(errors){
+                            var allErrors = {};
+                            for(var i = 0 ; i < errors.length; i++ ){
+                                allErrors[errors[i].field] = errors[i].message;
+                            }
+                            return allErrors;
+                        },
+                        getAreas(){
+                            var self = this;
+                            axios.get('/apv1/area?sort=-nombre&per-page=100')
+                                .then(function (response) {
+                                    // handle success
+                                    console.log(response.data);
+                                    console.log("trae todas las areas");
+                                    // self.especialidades = response.data;
+                                    self.areas = response.data;
+                                })
+                                .catch(function (error) {
+                                    // handle error
+                                    console.log(error);
+                                })
+                                .then(function () {
+                                    // always executed
+                                });
+                        },
+                        getEmpresas(){
+                            var self = this;
+                            axios.get('/apv1/empresa?sort=-nombre&per-page=100')
+                                .then(function (response) {
+                                    // handle success
+                                    console.log(response.data);
+                                    console.log("trae todas las empresa");
+                                    // self.especialidades = response.data;
+                                    self.empresas = response.data;
+                                })
+                                .catch(function (error) {
+                                    // handle error
+                                    console.log(error);
+                                })
+                                .then(function () {
+                                    // always executed
+                                });
+                        },
+                        getMotivos(){
+                            var self = this;
+                            axios.get('/apv1/motivo?sort=-nombre&per-page=100')
+                                .then(function (response) {
+                                    // handle success
+                                    console.log(response.data);
+                                    console.log("trae todas las motivo");
+                                    // self.especialidades = response.data;
+                                    self.motivos = response.data;
+                                })
+                                .catch(function (error) {
+                                    // handle error
+                                    console.log(error);
+                                })
+                                .then(function () {
+                                    // always executed
+                                });
+                        },
+                        getCategorias(){
+                            var self = this;
+                            axios.get('/apv1/categoria?sort=-nombre&per-page=100')
+                                .then(function (response) {
+                                    // handle success
+                                    console.log(response.data);
+                                    console.log("trae todas las categorias");
+                                    // self.especialidades = response.data;
+                                    self.categorias = response.data;
+                                })
+                                .catch(function (error) {
+                                    // handle error
+                                    console.log(error);
+                                })
+                                .then(function () {
+                                    // always executed
+                                });
+                        },
+                        getLocalidad(){
+                            var self = this;
+                            axios.get('/apv1/localidad?sort=-nombre&per-page=100')
+                                .then(function (response) {
+                                    // handle success
+                                    console.log(response.data);
+                                    console.log("trae todas las localidades");
+                                    // self.especialidades = response.data;
+                                    self.localidades = response.data;
+                                })
+                                .catch(function (error) {
+                                    // handle error
+                                    console.log(error);
+                                })
+                                .then(function () {
+                                    // always executed
+                                });
+                        },
+                       addActas:function(){
+                           var self = this;
+                           const params = new URLSearchParams();
+                           params.append('nro', self.nro);
+                           params.append('fec', self.fec);
+                           params.append('id_localidad', self.selected_localidad);
+                           params.append('id_categoria', self.selected_categoria);
+                           params.append('id_motivo', self.selected_motivo);
+                           params.append('id_empresa', self.selected_empresa);
+                           params.append('id_area', self.selected_area);
+                           params.append('latitud', self.latitud);
+                           params.append('longitud', self.longitud);
+                           axios.post('/apv1/actasinspeccion',params)
+                              .then(function (response) {
+                                  // handle success
+                                  console.log(response.data);
+                                  alert('Los datos fueron guardados');
+
+                              })
+                              .catch(function (error) {
+                                  // handle error
+                                  console.log(error.response.data);
+                                  self.errors = self.normalizeErrors(error.response.data);
+                              })
+                              .then(function () {
+                                  // always executed
+                              });
+                      }
                     }
+                                    
                    
                   })
 </script>
