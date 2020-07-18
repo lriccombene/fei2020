e895b9289fbb5f864b1893f47daa080a7f4401f5
Lucas Riccombene <lriccombene@gmail.com>
2020-07-18 15:28:37 -0300

    se agrego paso a paso de como inciar aplicacion

--- bin/start-project.sh
+++ bin/start-project.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker-compuse exec app composer global require "fxp/composer-asset-plugin:^1.4.1"
+docker-compose exec app composer global require "fxp/composer-asset-plugin:^1.4.1"
 
-docker-compuse exec app composer create-project --prefer-dist yiisoft/yii2-app-basic .
+docker-compose exec app composer create-project --prefer-dist yiisoft/yii2-app-basic .
 
