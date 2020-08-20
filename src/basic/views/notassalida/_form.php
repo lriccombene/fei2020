e61d603ec981db7a474a030e7e7eacd7ee778655
Lucas Riccombene <lriccombene@gmail.com>
2020-08-20 17:38:27 -0300

    se reparo la visual

--- src/basic/views/notassalida/_form.php
+++ src/basic/views/notassalida/_form.php
@@ -7,31 +7,37 @@ use yii\widgets\ActiveForm;
 /* @var $model app\models\Notassalida */
 /* @var $form yii\widgets\ActiveForm */
 use yii\web\View;
-$this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",['position'=>View::POS_HEAD]);
+Yii::$app->params['boostrap']=4;
+/* @var $this yii\web\View */
+/* @var $model app\models\Consultor */
+$this->registerCssFile("//unpkg.com/bootstrap/dist/css/bootstrap.min.css",['position'=>$this::POS_HEAD]);
+$this->registerCssFile("//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css",['position'=>$this::POS_HEAD]);
+//$this->registerCssFile("//polyfill.io/v3/polyfill.min.js?features=es2015%2CIntersectionObserver",['position'=>$this::POS_HEAD]);
+
+$this->registerJsFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js",['position'=>$this::POS_HEAD]);
+$this->registerJsFile("https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.js",['position'=>$this::POS_HEAD]);
+//$this->registerJsFile("https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue-icons.min.js",['position'=>$this::POS_HEAD]);
 $this->registerJsFile("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",['position'=>$this::POS_HEAD]);
 ?>
-
+   <p> </p>
+   <p> </p>
+   <p> </p>
 <div class="notassalida-form">
     <div id="app" class="container-fluid">
-            <div class="row">
-                <div class=class="form-group">
+
+                <div class="form-group">
                     <label for="fec_emision">Fecha emisión:</label>
 
                     <input v-bind:placeholder="fec_emision" class="form-control" id="fec_emision" v-model="fec_emision" type="date" name="fec_emision" required >
                     <span class="text-danger" v-if="errors.fec_emision" >{{errors.fec_emision}}</span>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class=class="form-group">
+                <div class="form-group">
                     <label for="fec_notificado">Fecha notificación :</label>
-
                     <input v-bind:placeholder="fec_notificado_hint" class="form-control" id="fec_notificado" v-model="fec_notificado" type="date" name="fec_notificado">
                     <span class="text-danger" v-if="errors.fec_notificado" >{{errors.fec_notificado}}</span>
                 </div>
-            </div>
-            <div class="row">
-                <div class=class="form-group">
+
+                <div class="form-group">
                     <label for="categoria">Categoría :</label>
 
                     <select v-model="selected_categoria" class="form-control" required >
@@ -41,9 +47,7 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",['p
                     </select>
                     <span class="text-danger" v-if="errors.id_categoria" >{{errors.id_categoria}}</span>
                 </div>
-            </div>
-            <div class="row">
-                <div class=class="form-group">
+                <div class="form-group">
                     <label for="empresa">Empresa :</label>
 
                 <select v-model="selected_empresa" class="form-control" required >
@@ -53,30 +57,27 @@ $this->registerJsFile("https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",['p
                 </select>
                 <span class="text-danger" v-if="errors.id_empresa" >{{errors.id_empresa}}</span>
                 </div>
-            </div>
-            <div class="row">
-                <div class=class="form-group">
+                <div class="form-group">
                     <label for="detalle">Detalle :</label>
-
                     <textarea v-bind:placeholder="detalle_hint" class="form-control" id="detalle" v-model="detalle" type="text" name="detalle"></textarea>
                     <span class="text-danger" v-if="errors.detalle" >{{errors.detalle}}</span>
                 </div>
-            </div>
-            <div class="row">
-                <div class=class="form-group">
-                    <label for="notificado">Notificado :</label>
 
+                <div class="form-group">
+                    <label for="notificado">Notificado :</label>
                     <input v-bind:placeholder="notificado_hint" class="form-control" id="notificado" v-model="notificado" type="text" name="notificado">
                     <span class="text-danger" v-if="errors.notificado" >{{errors.notificado}}</span>
                 </div>
-            </div>
+
             <p> </p>
-            <div class="row">
-                <div class=class="form-group">
+            <p> </p>
+            <p> </p>
+
+                <div class="form-group">
                     <button v-if="!id"  v-on:click="add()"  type ="button"  class="btn btn-success">Enviar</button>
                     <button v-if="id" v-on:click ="edit(id)" type ="button" class="btn btn-warning" >Actualizar</button>
                 </div>
-           </div>
+
     </div>
 
 
--- src/basic/views/notassalida/create.php
+++ src/basic/views/notassalida/create.php
@@ -11,7 +11,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="notassalida-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Crear Notas de Salida</h1>
 
     <?= $this->render('_form', [
         'model' => $model,
